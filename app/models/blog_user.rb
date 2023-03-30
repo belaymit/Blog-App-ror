ef2fb78e8a5c1a588a5d821508f86a647a231072
belaymit <2belamit@gmail.com>
2023-03-30 13:07:33 +0300

    Deleted unwante files

--- app/models/blog_user.rb
+++
@@ -1,9 +0,0 @@
-class BlogUser < ApplicationRecord
-  has_many :posts, foreign_key: 'blog_user_id'
-  has_many :comments, foreign_key: 'blog_user_id'
-  has_many :likes, foreign_key: 'blog_user_id'
-
-  def recent_three_posts
-    posts.order(created_at: :desc).limit(3)
-  end
-end
