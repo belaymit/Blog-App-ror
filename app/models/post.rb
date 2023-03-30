461faef2c35ec412213a3b91fd0d1e7667d26a4c
belaymit <2belamit@gmail.com>
2023-03-30 09:12:09 +0300

    Modified post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :blog_user, foreign_key => 'blog_user_id'
+  belongs_to :blog_user
   has_many :comments
+  has_many :likes
+
+  after_save :update_posts_counter
+
+  def recent_five_comments
+    comments.last(5)
+  end
+
+  private
+
+  def update_posts_counter
+    blog_user.update(posts_counter: blog_user.posts.count)
+  end
 end
