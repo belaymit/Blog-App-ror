57a31aa1444db9d1ab58b1901f2fdd950798b46f
belaymit <2belamit@gmail.com>
2023-03-30 09:10:51 +0300

    Added blog_user model

--- app/models/blog_user.rb
+++ app/models/blog_user.rb
@@ -1,4 +1,10 @@
 class BlogUser < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  has_many :posts, foreign_key: 'blog_user_id'
+  has_many :comments, foreign_key: 'blog_user_id'
+  has_many :likes, foreign_key: 'blog_user_id'
+
+  def recent_three_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+  
 end
