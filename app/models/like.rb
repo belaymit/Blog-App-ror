1bbaa167985c87d2ce6db9691a18b9e81490868f
belaymit <2belamit@gmail.com>
2023-03-30 09:11:45 +0300

    Modified Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :blog_user
   belongs_to :post
+
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.update(likes_count: post.likes.count)
+  end
 end
