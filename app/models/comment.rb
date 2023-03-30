dcd7660a1c8344da9f82dd481eb90f113d69ae9d
belaymit <2belamit@gmail.com>
2023-03-30 09:11:24 +0300

    Modified Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :blog_user
   belongs_to :post
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(coments_count: post.comments.count)
+  end
 end
