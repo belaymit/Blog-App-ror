b9247f04b5344bcbc888a232669f65ec54769c73
belaymit <2belamit@gmail.com>
2023-03-31 08:34:45 +0300

    Added test case for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Like.new(author_id: 1, post_id: 1) }
+
+  before { subject.save }
+
+  it 'Like should have an author' do
+    subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Like should have a post' do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'counter method should be called after saving' do
+    author = User.create(name: 'Miliyon', posts_counter: 0)
+    post = Post.create(title: 'Firts post', author_id: author.id, comments_counter: 0, likes_counter: 0)
+    Like.create(author_id: author.id, post_id: post.id)
+    lastpost = Post.last
+    expect(lastpost.likes_counter).to eq(1)
+  end
 end
