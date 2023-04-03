635268be1635575d80cff01df1ff1cc5df90e399
belaymit <2belamit@gmail.com>
2023-04-03 23:32:41 +0300

    Modified destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,11 +15,10 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.author = current_user
 
     if @post.save
       flash[:success] = 'Post created successfully'
-      redirect_to user_posts_path(current_user)
+      redirect_to roo_path
     else
       flash[:error] = 'The was an error while creating post'
       render :new
@@ -27,11 +26,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id]).destroy
-
-    respond_to do |format|
-      format.html { redirect_to user_path(current_user), notice: 'Post destroyed successfully!' }
-    end
+    # to do
   end
 
   private
