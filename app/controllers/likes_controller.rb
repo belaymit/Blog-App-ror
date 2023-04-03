18440436ff37364ab2e5a4052e6c174399e257ec
belaymit <2belamit@gmail.com>
2023-04-03 23:32:11 +0300

    Modified likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,10 @@ class LikesController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
     @like = Like.new
-    @like.author = current_user
     @like.post = @post
 
     if @like.save
-      redirect_to user_post_path(@user, @post)
+      redirect_to roo_path
     else
       render 'posts/show'
     end
