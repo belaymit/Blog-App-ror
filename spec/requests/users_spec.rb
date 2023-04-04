e1acde0966b1c02b61f1ffabb04bd6efe655cb7c
belaymit <2belamit@gmail.com>
2023-04-04 22:07:41 +0300

    Added test case for user_controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Belay', photo: 'https://i.imgur.com/1JZ1Q2r.jpg', bio: 'This is my biography',
+      posts_counter: 2)
+    end
+   describe 'GET #index' do
+    before(:each) { get users_path(@user) }
+
+    it 'Should be success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' view" do
+      expect(response).to render_template(:index)
+    end
+
+    it 'Should return correct placeholder text' do
+      expect(response.body).to include('All Users')
+    end
+
+    it "does not render a 'show' view" do
+      expect(response).to_not render_template(:show)
+    end
+  end
+  describe 'GET #show' do
+    before(:each) { get user_path(@user) }
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' view" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'Should return correct placeholder text' do
+      expect(response.body).to include('Users Details')
+    end
   end
 end
