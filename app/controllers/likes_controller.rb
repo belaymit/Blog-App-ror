2510983a1a31179b4a494347cc3bedf3758e0fb5
belaymit <2belamit@gmail.com>
2023-04-07 08:36:30 +0300

    Modified likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,14 @@
 class LikesController < ApplicationController
   def create
-    # create likes
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @like = Like.new
+    @like.author = current_user
+    @like.post = @post
+    if @like.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render 'posts/show'
+    end
   end
 end
