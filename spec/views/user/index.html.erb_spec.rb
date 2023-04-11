1704136c747a909b1434fcf7c8684eb58052148e
belaymit <2belamit@gmail.com>
2023-04-11 18:20:29 +0300

    Fixed rubocop error

--- spec/views/user/index.html.erb_spec.rb
+++ spec/views/user/index.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'User index page', type: :feature do
     before(:each) do
       @user = User.create(name: 'Belay', photo: 'https://avatars.githubusercontent.com/u/27350247?v=4',
                           bio: 'I am engineer', posts_counter: 2)
-        visit users_path
+      visit users_path
     end
 
     it 'can see the username of all other users' do
@@ -18,7 +18,7 @@ RSpec.describe 'User index page', type: :feature do
     end
     it ' clicking on a user redirects to that user\'s show page' do
       click_link(@user.name)
-      expect(page).to have_current_path(user_post_path(@user.id))
+      expect(page).to have_current_path(user_path(@user.id))
     end
   end
 end
