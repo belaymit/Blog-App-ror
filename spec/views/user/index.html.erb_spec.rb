52a4eebae5ccb4950b1a2cd5dc6e0c184436d6ea
belaymit <2belamit@gmail.com>
2023-04-11 18:12:17 +0300

    Refactored user index view

--- spec/views/user/index.html.erb_spec.rb
+++ spec/views/user/index.html.erb_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe 'User index page', type: :feature do
     before(:each) do
       @user = User.create(name: 'Belay', photo: 'https://avatars.githubusercontent.com/u/27350247?v=4',
                           bio: 'I am engineer', posts_counter: 2)
-      @user2 = User.create(name: 'Name', photo: 'https://avatars.githubusercontent.com/u/27350247?v=4',
-                           bio: 'I am student', posts_counter: 3)
-      @post1 = Post.create(author: @user, title: 'My first post', text: 'This is my first post',
-                           comments_counter: 0, likes_counter: 0)
-      Comment.create(post: @post1, author: @user2, text: 'This the first comment on post')
-      visit user_posts_path(@user)
+        visit users_path
     end
 
     it 'can see the username of all other users' do
@@ -22,8 +17,8 @@ RSpec.describe 'User index page', type: :feature do
       expect(page.body).to have_content(@user.posts_counter.to_s)
     end
     it ' clicking on a user redirects to that user\'s show page' do
-      click_link(@post1.title)
-      expect(page).to have_current_path(user_post_path(@user, @post1))
+      click_link(@user.name)
+      expect(page).to have_current_path(user_post_path(@user.id))
     end
   end
 end
