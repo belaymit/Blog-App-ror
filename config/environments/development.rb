4c4af504804d2a3e1618126e4259c77e3003fb03
belaymit <2belamit@gmail.com>
2023-04-12 21:11:16 +0300

    Added letter-opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,17 +69,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   user_name:      'gmail_mail',
-  #   password:       'email_password',
-  #   domain:         'localhost:3000',
-  #   address:       'smtp.gmail.com',
-  #   port:          '587',
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
